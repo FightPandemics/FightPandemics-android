0844efafc0b5ab85459115272a9b0dc430b5f89b
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2020-05-18 22:50:43 +0100

    add dagger

--- app/src/main/java/com/fightpandemics/MainActivity.kt
+++ app/src/main/java/com/fightpandemics/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.fightpandemics
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.Component
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject lateinit var info: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+}
+
+class Info @Inject constructor() {
+    val text = "Hello Dagger 2"
+}
+
+@Component
+interface MagicBox {
+    fun poke(app: MainActivity)
 }
