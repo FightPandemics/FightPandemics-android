e3cb525c0965f576088fe91f985657b052f90de9
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2020-05-21 00:51:07 +0100

    update

--- app/src/main/java/com/fightpandemics/MainActivity.kt
+++ app/src/main/java/com/fightpandemics/MainActivity.kt
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import dagger.Component
 import javax.inject.Inject
 
+
 class MainActivity : AppCompatActivity() {
 
-    @Inject lateinit var info: Info
+    @Inject
+    lateinit var info: Info
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
 
 class Info @Inject constructor() {
     val text = "Hello Dagger 2"
