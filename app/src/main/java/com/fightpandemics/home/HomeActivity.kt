ab63cd0420e74092ae56727bf09897a1f5c170eb
Rajas Gandhi <rajasmgandhi@gmail.com>
2020-06-17 21:26:31 -0500

    Update HomeActivity.kt

--- app/src/main/java/com/fightpandemics/home/HomeActivity.kt
+++ app/src/main/java/com/fightpandemics/home/HomeActivity.kt
@@ -46,7 +46,7 @@ class HomeActivity : BaseActivity(), HomeContract.View {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        injectDependencies()
+        //injectDependencies()
         initAnims()
         initFabActions()
         setupUi()
