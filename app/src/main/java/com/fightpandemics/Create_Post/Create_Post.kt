dce334d3c227706b9f85f7f8ca05e30d9ad773e3
Daniel <deromyoha@gmail.com>
2020-07-03 14:54:54 +0100

    Created Post Activity for posting and added new svg icons to the drawable folder also made layouts for the bottom sheet dialog for post visibbility and offer/request

---
+++ app/src/main/java/com/fightpandemics/Create_Post/Create_Post.kt
@@ -0,0 +1,12 @@
+package com.fightpandemics.Create_Post
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fightpandemics.R
+
+class Create_Post : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create__post)
+    }
+}
