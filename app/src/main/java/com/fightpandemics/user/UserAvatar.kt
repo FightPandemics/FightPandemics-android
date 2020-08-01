d57f1a2cbb63718a665d648751dd7789c8c49fff
rajasgandhi <rajasmgandhi@gmail.com>
2020-08-01 10:03:35 -0500

    added xml support for initials and user image

--- app/src/main/java/com/fightpandemics/user/UserAvatar.kt
+++ app/src/main/java/com/fightpandemics/user/UserAvatar.kt
@@ -12,39 +12,74 @@ import com.android.volley.Response
 import com.android.volley.toolbox.ImageRequest
 import com.android.volley.toolbox.Volley
 import com.fightpandemics.R
-
+import java.lang.RuntimeException
 
 class UserAvatar : RelativeLayout {
     private var mInflater: LayoutInflater
+    private var imageUrl : String
+    private var userInitials : String
 
     constructor(context: Context?) : super(context) {
         mInflater = LayoutInflater.from(context)
-        init("NN")
+        imageUrl = ""
+        userInitials = "NN"
     }
 
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         mInflater = LayoutInflater.from(context)
+        imageUrl = ""
+        userInitials = ""
+        if(attrs != null) {
+            val attributes = context.obtainStyledAttributes(attrs, R.styleable.UserAvatar, 0, 0)
+            try {
+                this.imageUrl = attributes.getString(R.styleable.UserAvatar_imageUrl).toString()
+                this.userInitials = attributes.getString(R.styleable.UserAvatar_userInitials).toString()
+            } finally {
+                attributes.recycle()
+            }
+        } else {
+            throw RuntimeException("AttributeSet is null!")
+        }
     }
 
-    fun init(userNameInitials: String, userAvatarURL: String, context: Context) {
+    fun setUpUserAvatar(context: Context) {
         val v: View = mInflater.inflate(R.layout.user_avatar_view, this, true)
-        val userImg = v.findViewById(R.id.customViewImg) as ImageView
-        val userInitials = v.findViewById(R.id.textView1) as TextView
-        val request = ImageRequest(userAvatarURL,
-            Response.Listener<Bitmap?> { b -> userImg.setImageBitmap(b) }, 0, 0, null,
+        val userImg = v.findViewById(R.id.userAvatarImage) as ImageView
+        val userInitials = v.findViewById(R.id.userAvatarInitials) as TextView
+
+        val imageRequest = ImageRequest(this.imageUrl, Response.Listener {
+            userImg.setImageBitmap(it)
+        }, userImg.layoutParams.width, userImg.layoutParams.height, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.ARGB_8888,
+            Response.ErrorListener {
+                userImg.visibility = View.GONE
+                userInitials.text = this.userInitials
+            })
+
+        val rQueue = Volley.newRequestQueue(context)
+        rQueue.add(imageRequest)
+    }
+
+    fun setUpUserAvatar(userNameInitials: String, userAvatarURL: String, context: Context) {
+        val v: View = mInflater.inflate(R.layout.user_avatar_view, this, true)
+        val userImg = v.findViewById(R.id.userAvatarImage) as ImageView
+        val userInitials = v.findViewById(R.id.userAvatarInitials) as TextView
+
+        val imageRequest = ImageRequest(userAvatarURL, Response.Listener {
+            userImg.setImageBitmap(it)
+        }, userImg.layoutParams.width, userImg.layoutParams.height, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.ARGB_8888,
             Response.ErrorListener {
                 userImg.visibility = View.GONE
                 userInitials.text = userNameInitials
             })
 
         val rQueue = Volley.newRequestQueue(context)
-        rQueue.add(request)
+        rQueue.add(imageRequest)
     }
 
-    fun init(userNameInitials: String) {
+    fun setUpUserAvatar(userNameInitials: String) {
         val v: View = mInflater.inflate(R.layout.user_avatar_view, this, true)
-        val userInitials = v.findViewById(R.id.textView1) as TextView
-        val userImg = v.findViewById(R.id.customViewImg) as ImageView
+        val userInitials = v.findViewById(R.id.userAvatarInitials) as TextView
+        val userImg = v.findViewById(R.id.userAvatarImage) as ImageView
         userImg.visibility = View.GONE
         userInitials.text = userNameInitials
     }
