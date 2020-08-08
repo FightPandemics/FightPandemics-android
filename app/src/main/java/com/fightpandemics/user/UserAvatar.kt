91d0c69fbe0ffde3467f46292d60472683e3a710
Rajas Gandhi <rajasmgandhi@gmail.com>
2020-08-08 10:38:38 -0500

    updated formatting

--- app/src/main/java/com/fightpandemics/user/UserAvatar.kt
+++ app/src/main/java/com/fightpandemics/user/UserAvatar.kt
@@ -12,12 +12,11 @@ import com.android.volley.Response
 import com.android.volley.toolbox.ImageRequest
 import com.android.volley.toolbox.Volley
 import com.fightpandemics.R
-import java.lang.RuntimeException
 
 class UserAvatar : RelativeLayout {
     private var mInflater: LayoutInflater
-    private var imageUrl : String
-    private var userInitials : String
+    private var imageUrl: String
+    private var userInitials: String
 
     constructor(context: Context?) : super(context) {
         mInflater = LayoutInflater.from(context)
@@ -29,16 +28,17 @@ class UserAvatar : RelativeLayout {
         mInflater = LayoutInflater.from(context)
         imageUrl = ""
         userInitials = ""
-        if(attrs != null) {
+        if (attrs != null) {
             val attributes = context.obtainStyledAttributes(attrs, R.styleable.UserAvatar, 0, 0)
             try {
                 this.imageUrl = attributes.getString(R.styleable.UserAvatar_imageUrl).toString()
-                this.userInitials = attributes.getString(R.styleable.UserAvatar_userInitials).toString()
+                this.userInitials =
+                    attributes.getString(R.styleable.UserAvatar_userInitials).toString()
             } finally {
                 attributes.recycle()
             }
         } else {
-            throw RuntimeException("AttributeSet is null!")
+            throw NullPointerException("AttributeSet is null!")
         }
     }
 
@@ -47,13 +47,19 @@ class UserAvatar : RelativeLayout {
         val userImg = v.findViewById(R.id.userAvatarImage) as ImageView
         val userInitials = v.findViewById(R.id.userAvatarInitials) as TextView
 
-        val imageRequest = ImageRequest(userAvatarURL, Response.Listener {
-            userImg.setImageBitmap(it)
-        }, userImg.layoutParams.width, userImg.layoutParams.height, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.ARGB_8888,
+        val imageRequest = ImageRequest(
+            userAvatarURL,
+            Response.Listener {
+                userImg.setImageBitmap(it)
+            }, userImg.layoutParams.width,
+            userImg.layoutParams.height,
+            ImageView.ScaleType.CENTER_CROP,
+            Bitmap.Config.ARGB_8888,
             Response.ErrorListener {
                 userImg.visibility = View.GONE
                 userInitials.text = userNameInitials
-            })
+            }
+        )
 
         val rQueue = Volley.newRequestQueue(this.context)
         rQueue.add(imageRequest)
