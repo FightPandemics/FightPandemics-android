7040094447498adb0d7ef6f99e95e6c01f4a7286
mayorjay <joseolu4gsm@yahoo.com>
2020-08-10 01:17:13 +0100

    Feat: Setup the di package

---
+++ app/src/main/java/com/fightpandemics/di/Modules.kt
@@ -0,0 +1,29 @@
+package com.fightpandemics.di
+
+import com.fightpandemics.data.local.AppDatabase
+import com.fightpandemics.data.remote.ApiService
+import com.fightpandemics.di.providers.AppDatabaseProvider
+import com.fightpandemics.di.providers.HttpLoggingInterceptorProvider
+import com.fightpandemics.di.providers.OkHttpClientProvider
+import com.fightpandemics.di.providers.RetrofitProvider
+import org.koin.dsl.module
+import retrofit2.converter.gson.GsonConverterFactory
+
+val dbModule = module {
+    single { AppDatabaseProvider.provide(get()) }
+}
+
+val daoModule = module {
+    single { get<AppDatabase>().dao() }
+}
+
+val networkModule = module {
+    single { HttpLoggingInterceptorProvider.provide() }
+    single { OkHttpClientProvider.provide(get()) }
+    single { GsonConverterFactory.create() }
+    single { RetrofitProvider.provide(get(), get()) }
+}
+
+val apiServiceModule = module {
+    single { ApiService.create(get()) }
+}
---
+++ app/src/main/java/com/fightpandemics/di/providers/RetrofitProvider.kt
@@ -0,0 +1,15 @@
+package com.fightpandemics.di.providers
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitProvider {
+
+    fun provide(converterFactory: GsonConverterFactory, client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+        .addConverterFactory(converterFactory)
+        .client(client)
+        .baseUrl("")
+        .build()
+}
