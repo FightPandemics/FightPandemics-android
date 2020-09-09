a1721004128e4af1beb91b9e19d00227ddc91531
mayorjay <joseolu4gsm@yahoo.com>
2020-09-10 00:57:36 +0100

    feat: Create OnBoarding Activity

---
+++ app/src/main/java/com/fightpandemics/ui/OnBoardingActivity.kt
@@ -0,0 +1,12 @@
+package com.fightpandemics.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fightpandemics.R
+
+class OnBoardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
