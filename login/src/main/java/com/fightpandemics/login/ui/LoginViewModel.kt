e6cdb6577d5a2d789cffb683180928e448f802e0
carlos_igor <igortkd2006@gmail.com>
2020-09-22 13:46:44 -0300

    login module injection

---
+++ login/src/main/java/com/fightpandemics/login/ui/LoginViewModel.kt
@@ -0,0 +1,8 @@
+package com.fightpandemics.login.ui
+
+import androidx.lifecycle.ViewModel
+import com.fightpandemics.dagger.scope.FeatureScope
+import javax.inject.Inject
+
+@FeatureScope
+class LoginViewModel @Inject constructor() : ViewModel()
