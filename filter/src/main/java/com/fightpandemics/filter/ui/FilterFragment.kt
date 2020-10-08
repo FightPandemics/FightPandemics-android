d02feaf271dbec56d61ffc81c0527c978eb69bc9
jaliquiel <jaliquiel@gmail.com>
2020-10-08 10:30:09 -0700

    Added functionality for applied text on from whom card, also corrected functionality on the location card

--- filter/src/main/java/com/fightpandemics/filter/ui/FilterFragment.kt
+++ filter/src/main/java/com/fightpandemics/filter/ui/FilterFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -15,6 +16,8 @@ import com.fightpandemics.home.databinding.FilterStartFragmentBinding
 import com.fightpandemics.utils.ViewModelFactory
 import com.google.android.material.transition.MaterialSharedAxis
 import kotlinx.android.synthetic.main.filter_location_options.view.*
+import kotlinx.android.synthetic.main.filter_start_fragment.view.*
+import timber.log.Timber
 import javax.inject.Inject
 
 class FilterFragment : Fragment() {
@@ -58,10 +61,10 @@ class FilterFragment : Fragment() {
                 setOnClickListener {
                     toggleContents(binding.locationOptions.root, binding.filterLocationExpandable.locationEmptyCard)
                     var selectedLocationQuery = binding.locationOptions.root.location_search.query
-                    if (selectedLocationQuery.isNotEmpty()){
-                        binding.filterLocationExpandable.selectedChipsText.visibility = View.VISIBLE
+                    if (selectedLocationQuery.isNotEmpty() && ! binding.root.location_options.isVisible){
+                        binding.filterLocationExpandable.filtersAppliedText.visibility = View.VISIBLE
                     }else{
-                        binding.filterLocationExpandable.selectedChipsText.visibility = View.GONE
+                        binding.filterLocationExpandable.filtersAppliedText.visibility = View.GONE
                     }
                 }
             }
@@ -70,8 +73,14 @@ class FilterFragment : Fragment() {
         binding.filterFromWhomExpandable.apply {
             this.fromWhomEmptyCard.apply {
                 setOnClickListener {
-                    val selectedChips = binding.fromWhomOptions.fromWhomChipGroup.checkedChipIds
+                    val selectedChips = binding.fromWhomOptions.fromWhomChipGroup.checkedChipIds.size
                     toggleContents(binding.fromWhomOptions.root, binding.filterFromWhomExpandable.fromWhomEmptyCard)
+                    if (! binding.root.from_whom_options.isVisible && selectedChips > 0){
+                        binding.filterFromWhomExpandable.filtersAppliedText.visibility = View.VISIBLE
+                        binding.filterFromWhomExpandable.filtersAppliedText.text = "${selectedChips} applied"
+                    }else{
+                        binding.filterFromWhomExpandable.filtersAppliedText.visibility = View.GONE
+                    }
                 }
             }
         }
