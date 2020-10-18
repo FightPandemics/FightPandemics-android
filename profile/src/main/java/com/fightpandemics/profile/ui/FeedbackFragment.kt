ae6bbb4348bdbcaef9bbf4e3bf4f1694227ff526
jaliquiel <jaliquiel@gmail.com>
2020-10-17 21:38:57 -0700

    Added functionality for highlighting material cards when focused

--- profile/src/main/java/com/fightpandemics/profile/ui/FeedbackFragment.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/FeedbackFragment.kt
@@ -13,7 +13,9 @@ import com.fightpandemics.profile.R
 import com.fightpandemics.profile.dagger.inject
 import com.fightpandemics.profile.databinding.FeedbackFragmentBinding
 import com.fightpandemics.utils.ViewModelFactory
+import com.google.android.material.card.MaterialCardView
 import kotlinx.android.synthetic.main.feedback_fragment.view.*
+import kotlinx.android.synthetic.main.feedback_question1.view.*
 import kotlinx.android.synthetic.main.profile_toolbar.*
 import timber.log.Timber
 import javax.inject.Inject
@@ -24,6 +26,7 @@ class FeedbackFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
     lateinit var binding: FeedbackFragmentBinding
+    lateinit var feedbackViewModel: FeedbackViewModel
 
     companion object {
         fun newInstance() = FeedbackFragment()
@@ -41,35 +44,27 @@ class FeedbackFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        return inflater.inflate(R.layout.profile_fragment, container, false)
+
         binding = FeedbackFragmentBinding.inflate(inflater)
-//        binding = inflater.inflate(R.layout.feedback_fragment_test, container, false)
         return binding.root
     }
 
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-//
-//        overview.setOnClickListener {
-//            Timber.d("Overview")
-//        }
-//
-//        toolbar.setOnMenuItemClickListener {
-//
-//            when (it.itemId) {
-//                R.id.settings -> {
 
-//                    Timber.d("Settings")
-//                    true
-//                }
-//
-//                else -> {
-//                    super.onOptionsItemSelected(it)
-//                }
-//            }
-//        }
-//
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Get the viewmodel
+        feedbackViewModel = ViewModelProvider(this).get(FeedbackViewModel::class.java)
+
+        binding.feedbackQuestion1.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
+            feedbackViewModel.displayActiveBorder(binding.feedbackQuestion1.root, hasFocus)
+        }
+
+        binding.feedbackQuestion2.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
+            feedbackViewModel.displayActiveBorder(binding.feedbackQuestion2.root, hasFocus)
+        }
+
+    }
+
 
 }
--- profile/src/main/java/com/fightpandemics/profile/ui/FeedbackViewModel.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/FeedbackViewModel.kt
@@ -1,8 +1,22 @@
 package com.fightpandemics.profile.ui
 
+import android.view.View
 import androidx.lifecycle.ViewModel
 import com.fightpandemics.dagger.scope.FeatureScope
+import com.google.android.material.card.MaterialCardView
+import kotlinx.android.synthetic.main.feedback_question1.view.*
 import javax.inject.Inject
 
 @FeatureScope
-class FeedbackViewModel @Inject constructor() : ViewModel()
+class FeedbackViewModel @Inject constructor() : ViewModel(){
+
+    fun displayActiveBorder(materialCard: MaterialCardView, hasFocus: Boolean){
+
+        if (hasFocus){
+            materialCard.vertical_accent.visibility = View.VISIBLE
+        }else{
+            materialCard.vertical_accent.visibility = View.GONE
+        }
+
+    }
+}
