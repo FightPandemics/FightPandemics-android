74c2ceade3f0b8a32f64112e8966fed665c2a99a
jaliquiel <jaliquiel@gmail.com>
2020-10-18 21:41:16 -0700

    Squashed bug due to some xml files having same id names. Refactored and gave them unique ids

--- profile/src/main/java/com/fightpandemics/profile/ui/FeedbackFragment.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/FeedbackFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.fightpandemics.profile.R
@@ -14,7 +15,6 @@ import com.fightpandemics.profile.dagger.inject
 import com.fightpandemics.profile.databinding.FeedbackFragmentBinding
 import com.fightpandemics.utils.ViewModelFactory
 import com.google.android.material.card.MaterialCardView
-import kotlinx.android.synthetic.main.feedback_question1.view.*
 import javax.inject.Inject
 //import com.fightpandemics.profile.databinding
 
@@ -46,40 +46,39 @@ class FeedbackFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         // Get the viewmodel
         feedbackViewModel = ViewModelProvider(this).get(FeedbackViewModel::class.java)
 
-        binding.feedbackQuestion1.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            displayActiveBorder(binding.feedbackQuestion1.root, hasFocus)
-            displayFilledEditText(binding.feedbackQuestion1.root.your_answer_edit_text, hasFocus)
+        binding.feedbackQuestion1.yourAnswerEditText1.setOnFocusChangeListener { view, hasFocus ->
+            displayActiveBorder(binding.feedbackQuestion1.verticalAccent1, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion1.yourAnswerEditText1, hasFocus)
         }
 
-        binding.feedbackQuestion2.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            displayActiveBorder(binding.feedbackQuestion2.root, hasFocus)
-            displayFilledEditText(binding.feedbackQuestion2.root.your_answer_edit_text, hasFocus)
+        binding.feedbackQuestion2.yourAnswerEditText2.setOnFocusChangeListener { view, hasFocus ->
+            displayActiveBorder(binding.feedbackQuestion2.verticalAccent2, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion2.yourAnswerEditText2, hasFocus)
         }
 
-        binding.feedbackQuestion3.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            displayActiveBorder(binding.feedbackQuestion3.root, hasFocus)
-            displayFilledEditText(binding.feedbackQuestion3.root.your_answer_edit_text, hasFocus)
+        binding.feedbackQuestion3.yourAnswerEditText3.setOnFocusChangeListener { view, hasFocus ->
+            displayActiveBorder(binding.feedbackQuestion3.verticalAccent3, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion3.yourAnswerEditText3, hasFocus)
         }
 
-        binding.feedbackQuestion4.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            displayActiveBorder(binding.feedbackQuestion4.root, hasFocus)
-            displayFilledEditText(binding.feedbackQuestion4.root.your_answer_edit_text, hasFocus)
+        binding.feedbackQuestion4.yourAnswerEditText4.setOnFocusChangeListener { view, hasFocus ->
+            displayActiveBorder(binding.feedbackQuestion4.verticalAccent4, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion4.yourAnswerEditText4, hasFocus)
         }
 
     }
 
-    fun displayActiveBorder(materialCard: MaterialCardView, hasFocus: Boolean){
+    fun displayActiveBorder(verticalAccent: FrameLayout, hasFocus: Boolean){
         if (hasFocus){
-            materialCard.vertical_accent.visibility = View.VISIBLE
+            verticalAccent.visibility = View.VISIBLE
         }else{
-            materialCard.vertical_accent.visibility = View.GONE
+            verticalAccent.visibility = View.GONE
         }
     }
 
@@ -97,5 +96,6 @@ class FeedbackFragment : Fragment() {
             editText.setBackgroundTintList(ColorStateList.valueOf( resources.getColor(R.color.filled_edittext_color)))
         }
     }
+
 
 }
