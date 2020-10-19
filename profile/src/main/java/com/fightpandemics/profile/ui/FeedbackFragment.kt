424924b50bd7236e70bfd061a072a9f0254002c0
jaliquiel <jaliquiel@gmail.com>
2020-10-18 17:56:57 -0700

    Added method for changing background tint for edit views when they are filled

--- profile/src/main/java/com/fightpandemics/profile/ui/FeedbackFragment.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/FeedbackFragment.kt
@@ -1,12 +1,12 @@
 package com.fightpandemics.profile.ui
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.fightpandemics.profile.R
@@ -14,10 +14,7 @@ import com.fightpandemics.profile.dagger.inject
 import com.fightpandemics.profile.databinding.FeedbackFragmentBinding
 import com.fightpandemics.utils.ViewModelFactory
 import com.google.android.material.card.MaterialCardView
-import kotlinx.android.synthetic.main.feedback_fragment.view.*
 import kotlinx.android.synthetic.main.feedback_question1.view.*
-import kotlinx.android.synthetic.main.profile_toolbar.*
-import timber.log.Timber
 import javax.inject.Inject
 //import com.fightpandemics.profile.databinding
 
@@ -57,22 +54,48 @@ class FeedbackFragment : Fragment() {
         feedbackViewModel = ViewModelProvider(this).get(FeedbackViewModel::class.java)
 
         binding.feedbackQuestion1.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            feedbackViewModel.displayActiveBorder(binding.feedbackQuestion1.root, hasFocus)
+            displayActiveBorder(binding.feedbackQuestion1.root, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion1.root.your_answer_edit_text, hasFocus)
         }
 
         binding.feedbackQuestion2.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            feedbackViewModel.displayActiveBorder(binding.feedbackQuestion2.root, hasFocus)
+            displayActiveBorder(binding.feedbackQuestion2.root, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion2.root.your_answer_edit_text, hasFocus)
         }
 
         binding.feedbackQuestion3.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            feedbackViewModel.displayActiveBorder(binding.feedbackQuestion3.root, hasFocus)
+            displayActiveBorder(binding.feedbackQuestion3.root, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion3.root.your_answer_edit_text, hasFocus)
         }
 
         binding.feedbackQuestion4.root.your_answer_edit_text.setOnFocusChangeListener { view, hasFocus ->
-            feedbackViewModel.displayActiveBorder(binding.feedbackQuestion4.root, hasFocus)
+            displayActiveBorder(binding.feedbackQuestion4.root, hasFocus)
+            displayFilledEditText(binding.feedbackQuestion4.root.your_answer_edit_text, hasFocus)
         }
 
     }
 
+    fun displayActiveBorder(materialCard: MaterialCardView, hasFocus: Boolean){
+        if (hasFocus){
+            materialCard.vertical_accent.visibility = View.VISIBLE
+        }else{
+            materialCard.vertical_accent.visibility = View.GONE
+        }
+    }
+
+    fun displayFilledEditText(editText: EditText, hasFocus: Boolean){
+        /*
+        not text & focus = highlight
+        text & focus = highlight
+        text & no focus = highlight
+        not text & no focus = nothing
+         */
+        val answer = editText.text.toString()
+        if (answer == "" && !hasFocus){
+            editText.setBackgroundTintList(ColorStateList.valueOf(resources.getColor(R.color.empty_edittext_color) ))
+        }else{
+            editText.setBackgroundTintList(ColorStateList.valueOf( resources.getColor(R.color.filled_edittext_color)))
+        }
+    }
 
 }
--- profile/src/main/java/com/fightpandemics/profile/ui/FeedbackViewModel.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/FeedbackViewModel.kt
@@ -1,21 +1,14 @@
 package com.fightpandemics.profile.ui
 
+import android.content.res.ColorStateList
 import android.view.View
+import android.widget.EditText
 import androidx.lifecycle.ViewModel
 import com.fightpandemics.dagger.scope.FeatureScope
+import com.fightpandemics.profile.R
 import com.google.android.material.card.MaterialCardView
 import kotlinx.android.synthetic.main.feedback_question1.view.*
 import javax.inject.Inject
 
 @FeatureScope
-class FeedbackViewModel @Inject constructor() : ViewModel(){
-
-    fun displayActiveBorder(materialCard: MaterialCardView, hasFocus: Boolean){
-        if (hasFocus){
-            materialCard.vertical_accent.visibility = View.VISIBLE
-        }else{
-            materialCard.vertical_accent.visibility = View.GONE
-        }
-    }
-
-}
+class FeedbackViewModel @Inject constructor() : ViewModel(){ }
