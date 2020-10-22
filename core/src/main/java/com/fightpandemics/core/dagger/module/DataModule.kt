7211eda91c52fa0dee75d11c3a2baa58cfff0f16
Osaigbovo Odiase <odiase.osaigbovo@gmail.com>
2020-10-22 07:33:50 +0100

    remove BuildConfig is defined multiple times error msg

--- core/src/main/java/com/fightpandemics/core/dagger/module/DataModule.kt
+++ core/src/main/java/com/fightpandemics/core/dagger/module/DataModule.kt
@@ -8,14 +8,18 @@ import com.fightpandemics.core.domain.repository.LoginRepository
 import com.fightpandemics.core.domain.repository.PostsRepository
 import dagger.Module
 import dagger.Provides
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
 import javax.inject.Singleton
 
 /**
  * Dagger module to provide data functionalities.
  */
+@FlowPreview
+@ExperimentalCoroutinesApi
 @Module
 class DataModule {
-
+    
     @Singleton
     @Provides
     fun providePostsRepository(postsRemoteDataSource: PostsRemoteDataSource): PostsRepository =
