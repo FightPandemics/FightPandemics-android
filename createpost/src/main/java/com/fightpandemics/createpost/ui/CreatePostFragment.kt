a9172547af35b3640a613078b65c68f977dab870
mayorjay <joseolu4gsm@yahoo.com>
2020-11-22 19:03:02 +0100

    feat: Design Create Post layout

--- createpost/src/main/java/com/fightpandemics/createpost/ui/CreatePostFragment.kt
+++ createpost/src/main/java/com/fightpandemics/createpost/ui/CreatePostFragment.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.fightpandemics.core.utils.ViewModelFactory
-import com.fightpandemics.createpost.R
 import com.fightpandemics.createpost.dagger.inject
 import com.fightpandemics.createpost.databinding.FragmentCreatePostBinding
 import javax.inject.Inject
@@ -26,10 +27,6 @@ class CreatePostFragment : Fragment() {
         inject(this)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,8 +37,33 @@ class CreatePostFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupToolBar()
+        setupViews()
+    }
+
     override fun onDestroyView() {
         fragmentCreatePostBinding = null
         super.onDestroyView()
+    }
+
+    private fun setupToolBar() {
+        fragmentCreatePostBinding!!.createPostToolbar.createPostToolbar.setNavigationOnClickListener { findNavController().navigateUp() }
+    }
+
+    private fun setupViews() {
+        fragmentCreatePostBinding!!.name.setOnClickListener { showToast(it) }
+        fragmentCreatePostBinding!!.offer.setOnClickListener { showToast(it) }
+        fragmentCreatePostBinding!!.request.setOnClickListener { showToast(it) }
+        fragmentCreatePostBinding!!.people.setOnClickListener { showToast(it) }
+        fragmentCreatePostBinding!!.month.setOnClickListener { showToast(it) }
+        fragmentCreatePostBinding!!.tag.setOnClickListener { showToast(it) }
+        fragmentCreatePostBinding!!.post.setOnClickListener { showToast(it) }
+    }
+
+    private fun showToast(view: View) {
+        Toast.makeText(requireContext(), view.toString() + "was clicked", Toast.LENGTH_SHORT).show()
     }
 }
