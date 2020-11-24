9865fc8fbedb8fa0ad1f6696650bef88a0e78946
mayorjay <joseolu4gsm@yahoo.com>
2020-11-24 02:06:06 +0100

    feat: Setup Visibility Dialog Fragment

--- createpost/src/main/java/com/fightpandemics/createpost/ui/CreatePostFragment.kt
+++ createpost/src/main/java/com/fightpandemics/createpost/ui/CreatePostFragment.kt
@@ -45,6 +45,7 @@ class CreatePostFragment : Fragment() {
         setupToolBar()
         setupViews()
         observeDurationBottomDialog()
+        observeVisibilityBottomDialog()
     }
 
     override fun onDestroyView() {
@@ -60,19 +61,18 @@ class CreatePostFragment : Fragment() {
         fragmentCreatePostBinding!!.name.setOnClickListener { displayOrganizationBottomDialog() }
         fragmentCreatePostBinding!!.offer.setOnClickListener { }
         fragmentCreatePostBinding!!.request.setOnClickListener { }
-        fragmentCreatePostBinding!!.people.setOnClickListener { displayVisibilityBottomDialog() }
-        fragmentCreatePostBinding!!.month.setOnClickListener { findNavController().navigate(R.id.action_createPostFragment_to_selectDurationFragment) }
+        fragmentCreatePostBinding!!.people.setOnClickListener {
+            findNavController().navigate(R.id.action_createPostFragment_to_selectVisibilityFragment)
+        }
+        fragmentCreatePostBinding!!.month.setOnClickListener {
+            findNavController().navigate(R.id.action_createPostFragment_to_selectDurationFragment)
+        }
         fragmentCreatePostBinding!!.tag.setOnClickListener { displayTagBottomDialog() }
         fragmentCreatePostBinding!!.post.setOnClickListener { }
     }
 
     private fun displayOrganizationBottomDialog() {
         val fragment = SelectOrganizationFragment()
-        fragment.show(requireActivity().supportFragmentManager, fragment.tag)
-    }
-
-    private fun displayVisibilityBottomDialog() {
-        val fragment = SelectVisibilityFragment()
         fragment.show(requireActivity().supportFragmentManager, fragment.tag)
     }
 
@@ -98,6 +98,35 @@ class CreatePostFragment : Fragment() {
                     }
                     else -> {
                         fragmentCreatePostBinding!!.month.text = getString(R.string.forever)
+                    }
+                }
+            }
+        }
+        navBackStackEntry.lifecycle.addObserver(observer)
+        viewLifecycleOwner.lifecycle.addObserver(LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_DESTROY) {
+                navBackStackEntry.lifecycle.removeObserver(observer)
+            }
+        })
+    }
+
+    private fun observeVisibilityBottomDialog() {
+        val navBackStackEntry = findNavController().getBackStackEntry(R.id.createPostFragment)
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_RESUME
+                && navBackStackEntry.savedStateHandle.contains("visibility")) {
+                when (navBackStackEntry.savedStateHandle.get<String>("visibility")) {
+                    "My neighbourhood" -> {
+                        fragmentCreatePostBinding!!.people.text = getString(R.string.people_in_my_neighbourhood)
+                    }
+                    "My city" -> {
+                        fragmentCreatePostBinding!!.people.text = getString(R.string.people_in_my_city)
+                    }
+                    "My country" -> {
+                        fragmentCreatePostBinding!!.people.text = getString(R.string.people_in_my_country)
+                    }
+                    else -> {
+                        fragmentCreatePostBinding!!.people.text = getString(R.string.anyone)
                     }
                 }
             }
--- createpost/src/main/java/com/fightpandemics/createpost/ui/SelectVisibilityFragment.kt
+++ createpost/src/main/java/com/fightpandemics/createpost/ui/SelectVisibilityFragment.kt
@@ -4,15 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.fightpandemics.createpost.R
 import com.fightpandemics.createpost.databinding.FragmentSelectVisibilityBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class SelectVisibilityFragment : BottomSheetDialogFragment() {
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = SelectVisibilityFragment()
-    }
 
     private var fragmentSelectVisibilityBinding: FragmentSelectVisibilityBinding? = null
 
@@ -28,5 +25,96 @@ class SelectVisibilityFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        fragmentSelectVisibilityBinding!!.neighbourhood.setOnClickListener {
+            val visibilityText = fragmentSelectVisibilityBinding!!.neighbourhood.text.toString()
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("visibility", visibilityText)
+            fragmentSelectVisibilityBinding!!.neighbourhood.apply {
+                setTextColor(resources.getColor(R.color.colorWhite))
+                setBackgroundColor(resources.getColor(R.color.colorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.city.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.country.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.anyone.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+        }
+
+
+        fragmentSelectVisibilityBinding!!.city.setOnClickListener {
+            val visibilityText = fragmentSelectVisibilityBinding!!.city.text.toString()
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("visibility", visibilityText)
+            fragmentSelectVisibilityBinding!!.city.apply {
+                setTextColor(resources.getColor(R.color.colorWhite))
+                setBackgroundColor(resources.getColor(R.color.colorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.neighbourhood.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.country.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.anyone.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+        }
+
+
+        fragmentSelectVisibilityBinding!!.country.setOnClickListener {
+            val visibilityText = fragmentSelectVisibilityBinding!!.country.text.toString()
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("visibility", visibilityText)
+            fragmentSelectVisibilityBinding!!.country.apply {
+                setTextColor(resources.getColor(R.color.colorWhite))
+                setBackgroundColor(resources.getColor(R.color.colorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.neighbourhood.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.city.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.anyone.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+        }
+
+
+        fragmentSelectVisibilityBinding!!.anyone.setOnClickListener {
+            val visibilityText = fragmentSelectVisibilityBinding!!.anyone.text.toString()
+            findNavController().previousBackStackEntry?.savedStateHandle?.set("visibility", visibilityText)
+            fragmentSelectVisibilityBinding!!.anyone.apply {
+                setTextColor(resources.getColor(R.color.colorWhite))
+                setBackgroundColor(resources.getColor(R.color.colorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.neighbourhood.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.city.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+            fragmentSelectVisibilityBinding!!.country.apply {
+                setBackgroundColor(resources.getColor(R.color.fightPandemicsWhiteSmoke))
+                setTextColor(resources.getColor(R.color.textColorPrimary))
+            }
+        }
     }
 }
