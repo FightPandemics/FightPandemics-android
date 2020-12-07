a6647da94729d48727b585571d0d65f767834363
mayorjay <joseolu4gsm@yahoo.com>
2020-12-07 01:20:06 +0100

    feat: Setup Organization RecyclerView and Adapter

--- createpost/src/main/java/com/fightpandemics/createpost/ui/SelectOrganizationFragment.kt
+++ createpost/src/main/java/com/fightpandemics/createpost/ui/SelectOrganizationFragment.kt
@@ -4,6 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.fightpandemics.createpost.R
+import com.fightpandemics.createpost.adapter.Organization
+import com.fightpandemics.createpost.adapter.OrganizationAdapter
 import com.fightpandemics.createpost.databinding.FragmentSelectOrganizationBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_select_organization.*
@@ -11,6 +16,7 @@ import kotlinx.android.synthetic.main.fragment_select_organization.*
 class SelectOrganizationFragment : BottomSheetDialogFragment() {
 
     private var fragmentSelectOrganizationBinding: FragmentSelectOrganizationBinding? = null
+    private var organizationArray: Array<Organization>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +31,9 @@ class SelectOrganizationFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        buildData()
         setupView()
+        setupRv()
     }
 
     private fun setupView() {
@@ -35,5 +43,19 @@ class SelectOrganizationFragment : BottomSheetDialogFragment() {
         fragmentSelectOrganizationBinding!!.add.setOnClickListener {
             dismiss()
         }
+    }
+
+    private fun buildData() {
+        val org1 = Organization(R.drawable.ic_plus_sign, "Notion IO")
+        val org2 = Organization(R.drawable.ic_tag, "FightPandemics")
+        val org3 = Organization(R.drawable.ic_plus_sign, "Amazon Ltd")
+        val org4 = Organization(R.drawable.ic_tag, "Spaces Plc")
+        val org5 = Organization(R.drawable.ic_plus_sign, "Channels TV")
+        organizationArray = arrayOf(org1, org2, org3, org4, org5)
+    }
+
+    private fun setupRv() {
+        fragmentSelectOrganizationBinding!!.rvOrganization.layoutManager = LinearLayoutManager(requireContext())
+        fragmentSelectOrganizationBinding!!.rvOrganization.adapter = OrganizationAdapter(organizationArray!!)
     }
 }
