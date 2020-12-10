ac360e71207bfd9b077454c0f3d047878d2fb0a4
mayorjay <joseolu4gsm@yahoo.com>
2020-12-11 00:45:46 +0100

    feat: Setup Description Text Watcher; Setup observer for create post button

--- createpost/src/main/java/com/fightpandemics/createpost/ui/CreatePostViewModel.kt
+++ createpost/src/main/java/com/fightpandemics/createpost/ui/CreatePostViewModel.kt
@@ -1,9 +1,23 @@
 package com.fightpandemics.createpost.ui
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.fightpandemics.core.dagger.scope.ActivityScope
 import javax.inject.Inject
 
 @ActivityScope
 class CreatePostViewModel @Inject constructor() : ViewModel() {
+
+    var titleNotEmpty = MutableLiveData<Boolean>()
+    var descriptionNotEmpty = MutableLiveData<Boolean>()
+    var isTagSet = MutableLiveData<Boolean>()
+    var allDataFilled = MutableLiveData<Boolean>()
+
+    init {
+
+    }
+    
+    fun setDataFilled() {
+        allDataFilled.value = titleNotEmpty.value == true && descriptionNotEmpty.value == true && isTagSet.value == true
+    }
 }
