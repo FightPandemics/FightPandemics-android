dfad833e78e57775146c88bf32dda280dad1a88a
jaliquiel <jaliquiel@gmail.com>
2020-12-20 07:53:46 -0800

    Finished initial implementation of save social links

--- profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileSocialFragment.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileSocialFragment.kt
@@ -5,17 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.fightpandemics.core.data.model.profile.IndividualProfileResponse
+import com.fightpandemics.core.data.model.profile.PatchIndividualProfileRequest
+import com.fightpandemics.core.data.model.profile.RequestUrls
 import com.fightpandemics.core.utils.ViewModelFactory
 import com.fightpandemics.profile.R
 import com.fightpandemics.profile.dagger.inject
-import kotlinx.android.synthetic.main.edit_profile_name_fragment.*
 import kotlinx.android.synthetic.main.edit_profile_social_fragment.*
-import kotlinx.android.synthetic.main.profile_toolbar.*
 import kotlinx.android.synthetic.main.profile_toolbar.toolbar
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
@@ -28,11 +27,6 @@ class EditProfileSocialFragment : Fragment() {
 
     @ExperimentalCoroutinesApi
     private val editProfileViewModel: EditProfileViewModel by viewModels { viewModelFactory }
-
-    // Obtain the ViewModel - use the ParentFragment as the Lifecycle owner
-    private val profileViewModel: ProfileViewModel
-            by viewModels({ requireParentFragment() }) { viewModelFactory }
-
 
     companion object {
         fun newInstance() = EditProfileSocialFragment()
@@ -65,9 +59,23 @@ class EditProfileSocialFragment : Fragment() {
         // todo save button
         // navigate up, then indicate profile view model to patch
 
+        social_links_save_button.setOnClickListener {
+            val urls = RequestUrls(
+                facebook = facebook_url_edittext.text.toString(),
+                github = github_url_edittext.text.toString(),
+                instagram = "this is missing in android",
+                linkedin = linkedin_url_edittext.text.toString(),
+                twitter = twitter_url_edittext.text.toString(),
+                website = website_url_edittext.text.toString()
+            )
+            editProfileViewModel.updateProfile(
+                PatchIndividualProfileRequest(currentProfile.about, urls)
+            )
+        }
+
     }
 
-    private fun bindSocialLinks(profile: IndividualProfileResponse){
+    private fun bindSocialLinks(profile: IndividualProfileResponse) {
         facebook_url_edittext.setText(profile.urls.facebook)
         linkedin_url_edittext.setText(profile.urls.linkedin)
         twitter_url_edittext.setText(profile.urls.twitter)
--- profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileViewModel.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileViewModel.kt
@@ -1,32 +1,56 @@
 package com.fightpandemics.profile.ui.profile
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.fightpandemics.core.dagger.scope.FeatureScope
 import com.fightpandemics.core.data.model.profile.IndividualProfileResponse
+import com.fightpandemics.core.data.model.profile.PatchIndividualProfileRequest
 import com.fightpandemics.core.result.Result
-import com.fightpandemics.profile.domain.LoadCurrentUserUseCase
+import com.fightpandemics.profile.domain.UpdateCurrentUserUseCase
 import com.fightpandemics.profile.util.capitalizeFirstLetter
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.async
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
 @FeatureScope
 class EditProfileViewModel @Inject constructor(
-    //private val loadCurrentUserUseCase: LoadCurrentUserUseCase,
+//    private val loadCurrentUserUseCase: LoadCurrentUserUseCase,
+    private val updateCurrentUserUseCase: UpdateCurrentUserUseCase
 ) : ViewModel(){
-    fun getName(): String =
-        currentProfile?.firstName?.capitalizeFirstLetter()  + " " + currentProfile?.lastName?.capitalizeFirstLetter()
-
 
     lateinit var currentProfile: IndividualProfileResponse
 
+    private val _updateState = MutableLiveData<ProfileViewModel.IndividualProfileViewState>()
+    val updateState: LiveData<ProfileViewModel.IndividualProfileViewState> get() = _updateState
 
+    fun getName(): String =
+        currentProfile?.firstName?.capitalizeFirstLetter()  + " " + currentProfile?.lastName?.capitalizeFirstLetter()
+
+    fun updateProfile(updatedProfile: PatchIndividualProfileRequest){
+        _updateState.value?.isLoading = true
+        viewModelScope.launch {
+            async {
+                updateCurrentUserUseCase(updatedProfile)
+            }.await().collect {
+                when(it){
+                    is Result.Success -> {
+                        Timber.i("Debug: Update was a success: ${it.data}")
+                    }
+                    is Result.Error -> {
+                        Timber.i("Debug: Update was a failure: ${it.exception.message}")
+                    }
+                }
+            }
+        }
+
+
+    }
 
 
 
