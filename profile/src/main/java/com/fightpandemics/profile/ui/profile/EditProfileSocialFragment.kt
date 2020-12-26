e448ffb02e085de2a594c483883784a25eade167
igor silva <carlos.isilva@grupopan.com>
2020-12-26 01:54:02 -0300

    validate social no more than 15 chars

--- profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileSocialFragment.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileSocialFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.fightpandemics.core.data.model.profile.IndividualProfileResponse
 import com.fightpandemics.core.data.model.profile.PatchIndividualProfileRequest
@@ -15,16 +14,31 @@ import com.fightpandemics.core.data.model.profile.RequestUrls
 import com.fightpandemics.core.utils.ViewModelFactory
 import com.fightpandemics.profile.R
 import com.fightpandemics.profile.dagger.inject
+import com.google.android.material.textfield.TextInputEditText
+import com.mobsandgeeks.saripaar.annotation.Length
+import com.mobsandgeeks.saripaar.annotation.Max
+import com.mobsandgeeks.saripaar.annotation.Optional
 import kotlinx.android.synthetic.main.edit_profile_social_fragment.*
 import kotlinx.android.synthetic.main.profile_toolbar.toolbar
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
 
-class EditProfileSocialFragment : Fragment() {
+class EditProfileSocialFragment : BaseFragment() {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
+
+    @Length(max = 15, messageResId = R.string.error_size_social)
+    lateinit var etFfacebook: TextInputEditText
+    @Length(max = 15, messageResId = R.string.error_size_social)
+    lateinit var etGithub: TextInputEditText
+    @Length(max = 15, messageResId = R.string.error_size_social)
+    lateinit var etLlinkedin: TextInputEditText
+    @Length(max = 15, messageResId = R.string.error_size_social)
+    lateinit var etTwitter: TextInputEditText
+    @Length(max = 15, messageResId = R.string.error_size_social)
+    lateinit var etWwebsite: TextInputEditText
 
     @ExperimentalCoroutinesApi
     private val profileViewModel: ProfileViewModel by activityViewModels { viewModelFactory }
@@ -50,6 +64,12 @@ class EditProfileSocialFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        etFfacebook = facebook_url_edittext
+        etGithub = github_url_edittext
+        etLlinkedin = linkedin_url_edittext
+        etTwitter = twitter_url_edittext
+        etWwebsite = website_url_edittext
+
         toolbar.setNavigationOnClickListener {
             findNavController().navigateUp()
         }
@@ -59,6 +79,9 @@ class EditProfileSocialFragment : Fragment() {
         // todo save button
         // navigate up, then indicate profile view model to patch
         social_links_save_button.setOnClickListener {
+            validator.validate()
+        }
+        validationOk = {
             val urls = RequestUrls(
                 facebook = facebook_url_edittext.text.toString(),
                 github = github_url_edittext.text.toString(),
