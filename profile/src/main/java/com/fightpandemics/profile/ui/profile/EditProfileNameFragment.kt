1aeb56135bf45f33d6614893f350656fb09bde11
jaliquiel <jaliquiel@gmail.com>
2020-12-31 10:37:40 -0800

    Name fields can only be of max 30 chars

--- profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileNameFragment.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/profile/EditProfileNameFragment.kt
@@ -6,15 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import com.fightpandemics.core.data.model.profile.*
+import com.fightpandemics.core.data.model.profile.PatchIndividualAccountRequest
 import com.fightpandemics.core.utils.ViewModelFactory
 import com.fightpandemics.profile.R
 import com.fightpandemics.profile.dagger.inject
 import com.google.android.material.textfield.TextInputEditText
+import com.mobsandgeeks.saripaar.annotation.Length
 import com.mobsandgeeks.saripaar.annotation.NotEmpty
 import kotlinx.android.synthetic.main.edit_profile_name_fragment.*
 import kotlinx.android.synthetic.main.profile_toolbar.toolbar
@@ -30,11 +29,13 @@ class EditProfileNameFragment : BaseFragment() {
     @ExperimentalCoroutinesApi
     private val profileViewModel: ProfileViewModel by activityViewModels { viewModelFactory }
 
-    @NotEmpty(messageResId = R.string.error_empty_name)
+    @Length(min=0, max=30, messageResId = R.string.error_name_length)
+    @NotEmpty(messageResId = R.string.error_empty_first_name)
     lateinit var tvFirstName: TextInputEditText
-    @NotEmpty
-    lateinit var tvLastName: TextInputEditText
 
+    @Length(min=0, max=30, messageResId = R.string.error_name_length)
+    @NotEmpty(messageResId = R.string.error_empty_last_name)
+    lateinit var tvLastName: TextInputEditText
 
     companion object {
         fun newInstance() = EditProfileNameFragment()
