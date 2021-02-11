e922a3264305157a1353eea2c7c788f74ec1b5c3
Osaigbovo Odiase <odiase.osaigbovo@gmail.com>
2021-02-11 14:22:13 +0100

    add viewbinding for Search module

--- core/src/main/java/com/fightpandemics/core/data/interceptors/AccessTokenAuthenticator.kt
+++ core/src/main/java/com/fightpandemics/core/data/interceptors/AccessTokenAuthenticator.kt
@@ -8,7 +8,19 @@ import okhttp3.Route
 // Authenticator is only called when there is a 401 HTTP unauthorized error,
 class AccessTokenAuthenticator : Authenticator {
 
-    override fun authenticate(route: Route?, response: Response): Request? {
-        TODO("Not yet implemented")
+    override fun authenticate(route: Route?, response: Response): Request? = when {
+        response.retryCount > 4 -> null
+        else -> null
     }
+
+    private val Response.retryCount: Int
+        get() {
+            var currentResponse = priorResponse
+            var result = 0
+            while (currentResponse != null) {
+                result++
+                currentResponse = currentResponse.priorResponse
+            }
+            return result
+        }
 }
