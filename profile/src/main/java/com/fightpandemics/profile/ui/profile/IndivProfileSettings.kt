ad65749f8ed6b91fac34a1b425a0b35a342a99c0
Axel Dahlberg <axel.dahlberg12@gmail.com>
2021-02-16 11:39:33 +0100

    Refactored profile settings layouts and started adding event listeners

--- profile/src/main/java/com/fightpandemics/profile/ui/profile/IndivProfileSettings.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/profile/IndivProfileSettings.kt
@@ -1,6 +1,7 @@
 package com.fightpandemics.profile.ui.profile
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,12 +12,27 @@ import androidx.navigation.fragment.findNavController
 import com.fightpandemics.core.utils.ViewModelFactory
 import com.fightpandemics.profile.R
 import com.fightpandemics.profile.dagger.inject
+import com.fightpandemics.utils.webviewer.WebViewerActivity
 import kotlinx.android.synthetic.main.settings_signed_in.*
+import kotlinx.android.synthetic.main.settings_signed_in.aboutUsContainer as signInAboutUsContainer
+import kotlinx.android.synthetic.main.settings_signed_in.privacyPolicyContainer as signInPrivacyPolicyContainer
+import kotlinx.android.synthetic.main.settings_signed_in.supportContainer as signInSupportContainer
+import kotlinx.android.synthetic.main.settings_signed_out.*
+import kotlinx.android.synthetic.main.settings_signed_out.aboutUsContainer as signOutAboutUsContainer
+import kotlinx.android.synthetic.main.settings_signed_out.privacyPolicyContainer as signOutPrivacyPolicyContainer
+import kotlinx.android.synthetic.main.settings_signed_out.supportContainer as signOutSupportContainer
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
 
+object URLs {
+    const val ABOUT_US = "https://fightpandemics.com/about-us"
+    const val PRIVACY_POLICY = "https://fightpandemics.com/privacy-policy"
+    const val SUPPORT = "https://fightpandemics.com/faq"
+}
+
 class IndivProfileSettings : Fragment() {
+
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
@@ -31,17 +47,39 @@ class IndivProfileSettings : Fragment() {
     override fun onStart() {
         super.onStart()
         bindListeners()
+        profileViewModel.getIndividualProfile()
     }
 
+    @ExperimentalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.settings_signed_in, container, false)
+        return if (isUserLoggedIn()) {
+            inflater.inflate(R.layout.settings_signed_out, container, false)
+        } else {
+            inflater.inflate(R.layout.settings_signed_in, container, false)
+        }
+    }
+
+    @ExperimentalCoroutinesApi
+    private fun isUserLoggedIn(): Boolean {
+        return profileViewModel.individualProfile.value?.id != null
     }
 
     private fun bindListeners() {
+//        myAccountContainer.setOnClickListener {
+//            findNavController().navigate(com.fightpandemics.R.id.action_indivProfileSettings_to_signup)
+//        }
+
+        signInAboutUsContainer.setOnClickListener { openWebView(URLs.ABOUT_US)}
+        signOutAboutUsContainer.setOnClickListener { openWebView(URLs.ABOUT_US)}
+        signInPrivacyPolicyContainer.setOnClickListener { openWebView(URLs.PRIVACY_POLICY)}
+        signOutPrivacyPolicyContainer.setOnClickListener { openWebView(URLs.PRIVACY_POLICY)}
+        signInSupportContainer.setOnClickListener { openWebView(URLs.SUPPORT)}
+        signOutSupportContainer.setOnClickListener { openWebView(URLs.SUPPORT)}
+
 //        toolbar.setOnMenuItemClickListener {
 //            when (it.itemId) {
 //                R.id.settings -> {
@@ -57,10 +95,6 @@ class IndivProfileSettings : Fragment() {
 //            }
 //        }
 
-//        button3?.setOnClickListener {
-//            findNavController().navigate(com.fightpandemics.R.id.action_profileFragment_to_editProfileFragment)
-//        }
-//
 //        profileViewModel.individualProfile.observe(viewLifecycleOwner) { profile ->
 //            getIndividualProfileListener(profile)
 //        }
@@ -73,7 +107,13 @@ class IndivProfileSettings : Fragment() {
 //        }
     }
 
-    companion object {
-        fun newInstance() = IndivProfileSettings()
+    private fun openWebView(url: String?) {
+        val intent = Intent(requireContext(), WebViewerActivity::class.java)
+        intent.putExtra("url", url)
+        startActivity(intent)
     }
+
+   companion object {
+       fun newInstance() = IndivProfileSettings()
+   }
 }
