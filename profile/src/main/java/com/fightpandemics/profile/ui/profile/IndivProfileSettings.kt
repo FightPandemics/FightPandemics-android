73f86e6a63a6dd2273630bc6555c6a88f33f9a89
Axel Dahlberg <git@valleymnt.com>
2021-02-22 04:10:37 -0800

    Feature/settings (#195)
    * Refactored profile settings layouts and started adding event listeners
    
    * Added action when clicking My Account
    
    * Profile settings screens for logged in and logged out users
    
    * Merged settings_signed_in and settings_signed_out layouts to a single profile_settings and hide show Views depending on user signed in or not
    
    * All signed in specific buttons can now be GONE since others are now aligned to container layout
    
    * Refactored out function
    
    Co-authored-by: Axel Dahlberg <axel.dahlberg12@gmail.com>

--- profile/src/main/java/com/fightpandemics/profile/ui/profile/IndivProfileSettings.kt
+++ profile/src/main/java/com/fightpandemics/profile/ui/profile/IndivProfileSettings.kt
@@ -1,6 +1,7 @@
 package com.fightpandemics.profile.ui.profile
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,10 +12,17 @@ import androidx.navigation.fragment.findNavController
 import com.fightpandemics.core.utils.ViewModelFactory
 import com.fightpandemics.profile.R
 import com.fightpandemics.profile.dagger.inject
-import kotlinx.android.synthetic.main.settings_signed_in.*
+import com.fightpandemics.utils.webviewer.WebViewerActivity
+import kotlinx.android.synthetic.main.profile_settings.*
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import javax.inject.Inject
+
+object URLs {
+    const val ABOUT_US = "https://fightpandemics.com/about-us"
+    const val PRIVACY_POLICY = "https://fightpandemics.com/privacy-policy"
+    const val SUPPORT = "https://fightpandemics.com/faq"
+}
 
 class IndivProfileSettings : Fragment() {
     @Inject
@@ -31,49 +39,58 @@ class IndivProfileSettings : Fragment() {
     override fun onStart() {
         super.onStart()
         bindListeners()
+        if (profileViewModel.isUserSignedIn()) {
+            hideSignedOutViews()
+        } else {
+            hideSignedInViews()
+        }
     }
 
+    @ExperimentalCoroutinesApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.settings_signed_in, container, false)
-    }
+    ): View? = inflater.inflate(R.layout.profile_settings, container, false)
 
+    @ExperimentalCoroutinesApi
     private fun bindListeners() {
-//        toolbar.setOnMenuItemClickListener {
-//            when (it.itemId) {
-//                R.id.settings -> {
-//                    Timber.d("bindListeners: Settings")
-//                    findNavController()
-//                    .navigate(R.id.action_profileFragment_to_indivProfileSettings)
-//                    true
-//                }
-//
-//                else -> {
-//                    super.onOptionsItemSelected(it)
-//                }
-//            }
-//        }
-
-//        button3?.setOnClickListener {
-//            findNavController().navigate(com.fightpandemics.R.id.action_profileFragment_to_editProfileFragment)
-//        }
-//
-//        profileViewModel.individualProfile.observe(viewLifecycleOwner) { profile ->
-//            getIndividualProfileListener(profile)
-//        }
-
-//        updateAccountInfoContainer.setOnClickListener {
-//            findNavController().navigate(com.fightpandemics.R.id.action_indivProfileSettings_to_editAccountFragment)
-//        }
-//        toolbar.setNavigationOnClickListener {
-//            findNavController().navigateUp()
-//        }
+        toolbar.setOnClickListener { activity?.onBackPressed() }
+        updatePublicProfileContainer.setOnClickListener {
+            findNavController().navigate(com.fightpandemics.R.id.action_indivProfileSettings_to_editProfileFragment)
+        }
+        updateAccountInfoContainer.setOnClickListener {
+            findNavController().navigate(com.fightpandemics.R.id.action_indivProfileSettings_to_editAccountFragment)
+        }
+        setupNotificationSettingsContainer.setOnClickListener { TODO() }
+        myAccountContainer.setOnClickListener {
+            findNavController().navigate(com.fightpandemics.R.id.action_indivProfileSettings_to_nav_splash_onboard)
+        }
+        aboutUsContainer.setOnClickListener { openWebView(URLs.ABOUT_US) }
+        privacyPolicyContainer.setOnClickListener { openWebView(URLs.PRIVACY_POLICY) }
+        supportContainer.setOnClickListener { openWebView(URLs.SUPPORT) }
+        feedbackContainer.setOnClickListener { TODO() }
+        signoutContainer.setOnClickListener { TODO() }
     }
 
-    companion object {
-        fun newInstance() = IndivProfileSettings()
+    private fun hideSignedInViews() {
+        arrayOf(
+            updatePublicProfileContainer,
+            updateAccountInfoContainer,
+            setupNotificationSettingsContainer,
+            signoutContainer,
+        ).forEach {
+            it.visibility = View.GONE
+        }
+    }
+
+    private fun hideSignedOutViews() {
+        myAccountContainer.visibility = View.GONE
+    }
+
+    private fun openWebView(url: String?) {
+        val intent = Intent(requireContext(), WebViewerActivity::class.java)
+        intent.putExtra("url", url)
+        startActivity(intent)
     }
 }
