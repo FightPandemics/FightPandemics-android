e6fb627643cc7b22744e0f1e43c2266c1b5d017c
jaliquiel <jaliquiel@gmail.com>
2021-03-10 16:19:42 -0800

    Added loading progress bar logic and edited xml for post details

--- home/src/main/java/com/fightpandemics/home/ui/PostDetailsFragment.kt
+++ home/src/main/java/com/fightpandemics/home/ui/PostDetailsFragment.kt
@@ -5,10 +5,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.fightpandemics.core.data.model.post.PostDetail
 import com.fightpandemics.core.utils.GlideApp
 import com.fightpandemics.core.utils.ViewModelFactory
 import com.fightpandemics.home.dagger.inject
@@ -23,7 +23,8 @@ class PostDetailsFragment : Fragment() {
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
-    private val postDetailsViewModel: PostDetailsViewModel by viewModels({ requireParentFragment() }) { viewModelFactory }
+    private val postDetailsViewModel: PostDetailsViewModel
+        by viewModels({ requireParentFragment() }) { viewModelFactory }
 
     private var _binding: FragmentPostDetailsBinding? = null
 
@@ -69,34 +70,55 @@ class PostDetailsFragment : Fragment() {
         postDetailsViewModel.postDetailState.observe(
             viewLifecycleOwner,
             { state ->
-                binding.progressBar.progressBar.isVisible = state.isLoading
-                state.postDetailResponse?.post?.let { post ->
-                    binding.postLayout?.let { layout ->
-                        layout.objective.text = post.objective
-                        "Posted ${getPostCreated(post.createdAt)} ago".also { layout.timePosted.text = it }
-                        layout.postTitle.text = post.title
-                        layout.postContent.text = post.content
-                        layout.likesCount.text = post.likesCount.toString()
-                        layout.commentsCount.text = post.commentsCount.toString()
-                        layout.userFullName.text = post.author.name
-                        "${post.author.location.city}, ${post.author.location.state}, ${post.author.location.country}".also { binding.postLayout.userLocation.text = it }
-                        binding.postLayout.userAvatar?.let { imageView ->
-                            post.author?.let { author ->
-                                if (author.photo.isBlank()) {
-                                    imageView.setInitials(author.name.substring(0, 1))
-                                    imageView.invalidate()
-                                } else {
-                                    GlideApp
-                                        .with(requireContext())
-                                        .load(author.photo)
-                                        .centerCrop()
-                                        .into(imageView)
-                                }
-                            }
-                        }
+                when {
+                    state.isLoading -> bindLoading(state.isLoading)
+                    state.postDetailResponse != null -> {
+                        bindLoading(state.isLoading)
+                        bindPost(state.postDetailResponse.post)
+                    }
+                    state.errorMessage != null -> {
+                        bindLoading(state.isLoading)
                     }
                 }
             }
         )
+    }
+
+    private fun bindPost(post: PostDetail) {
+        binding.postLayout.apply {
+            objective.text = post.objective
+            "Posted ${getPostCreated(post.createdAt)} ago".also { timePosted.text = it }
+            postTitle.text = post.title
+            postContent.text = post.content
+            likesCount.text = post.likesCount.toString()
+            commentsCount.text = post.commentsCount.toString()
+            userFullName.text = post.author.name
+            "${post.author.location.city}, ${post.author.location.state}, ${post.author.location.country}".also {
+                binding.postLayout.userLocation.text = it
+            }
+
+            val imageView = binding.postLayout.userAvatar
+            val author = post.author
+            if (author.photo.isBlank()) {
+                imageView.setInitials(author.name.substring(0, 1))
+                imageView.invalidate()
+            } else {
+                GlideApp
+                    .with(requireContext())
+                    .load(author.photo)
+                    .centerCrop()
+                    .into(imageView)
+            }
+        }
+    }
+
+    private fun bindLoading(isLoading: Boolean) {
+        if (isLoading) {
+            binding.progressBar.visibility = View.VISIBLE
+            binding.postDetailsContent.visibility = View.GONE
+        } else {
+            binding.progressBar.visibility = View.GONE
+            binding.postDetailsContent.visibility = View.VISIBLE
+        }
     }
 }
